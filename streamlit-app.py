ad1f2355fc89dad7fd29eba58b3050db6723f7f2
GAETANNGALEU23 <francoisngaleu232@gmail.com>
2024-11-19 11:46:03 +0100

    Create streamlit-app.py

---
+++ streamlit-app.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+import pandas as pd
+import altair as alt 
+ 
+# Charger les données
+df = pd.read_csv('Iris.csv', delimiter=';')
+ 
+# Créer un titre
+st.title("Mon premier tableau de bord Streamlit")
+ 
+# Afficher les données dans un tableau
+#st.table(data) 
+st.write(df.head(3))
