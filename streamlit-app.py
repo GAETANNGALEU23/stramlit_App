1334035fad5959d030de8a1eeca563e0b87e0bfb
GAETANNGALEU23 <francoisngaleu232@gmail.com>
2024-11-19 12:31:06 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,7 +1,6 @@
 import streamlit as st 
 import pandas as pd
 import altair as alt 
-import plotly.express as px
  
 # Charger les données
 df = pd.read_csv('Iris.csv', delimiter=';')
@@ -12,5 +11,5 @@ st.title("Mon premier tableau de bord Streamlit")
 # Afficher les données dans un tableau
 # st.table(df) 
 st.write(df.head(5))
-fig = px.scatter(df, x='x', y='y')
-st.plotly_chart(fig)   
+ 
+   
