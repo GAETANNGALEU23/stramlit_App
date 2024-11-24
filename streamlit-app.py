fcc07a0c62884a5962d9237d542f0e719317ae7c
GAETANNGALEU23 <francoisngaleu232@gmail.com>
2024-11-24 14:28:34 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt 
 import seaborn as sn
+import matplotlib as mpl
  
 # Charger les données
 df = pd.read_csv('Iris.csv', delimiter=';')
