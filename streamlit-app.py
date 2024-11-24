7c7c12959a7dcd51a9e38952e4497da8fdc498fe
GAETANNGALEU23 <francoisngaleu232@gmail.com>
2024-11-24 14:42:27 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt 
-import seaborn as sn
 import matplotlib as mpl
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
